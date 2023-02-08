e775130ee0e99e8931586146bb4aaf62dd8a4c2d
Deory <deory.lee@gmail.com>
2023-02-08 22:33:36 +0900

    3. 조금 수정한다. 4. 테스트가 성공하는 것을 확인한다.

---
+++ src/main/java/com/deory/tdd_byexample/currency/Dollar.java
@@ -0,0 +1,15 @@
+package com.deory.tdd_byexample.currency;
+
+public class Dollar {
+    
+    int amount = 10;
+    
+    Dollar(int amount) {
+    
+    }
+    
+    void times(int multiplier) {
+    
+    }
+    
+}
