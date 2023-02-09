fb59d9755e01e54e1741f36d09c586feb1059097
Deory <chlee27@lguplus.co.kr>
2023-02-09 16:58:02 +0900

    5. 중복을 제거하기 위해 리팩토링을 한다. Dollar, Franc 의 생성자를 공통화

--- src/main/java/com/deory/tdd_byexample/currency/Franc.java
+++ src/main/java/com/deory/tdd_byexample/currency/Franc.java
@@ -2,13 +2,13 @@ package com.deory.tdd_byexample.currency;
 
 public class Franc extends Money {
     
-    Franc(int amount) {
+    Franc(int amount, String currency) {
         this.amount = amount;
-        currency = "CHF";
+        this.currency = currency;
     }
     
     Money times(int multiplication) {
-        return new Franc(amount * multiplication);
+        return new Franc(amount * multiplication, null);
     }
     
 }
--- src/main/java/com/deory/tdd_byexample/currency/Money.java
+++ src/main/java/com/deory/tdd_byexample/currency/Money.java
@@ -15,11 +15,11 @@ public abstract class Money {
     }
     
     static Dollar dollar(int amount) {
-        return new Dollar(amount);
+        return new Dollar(amount, "USD");
     }
     
     static Franc franc(int amount) {
-        return new Franc(amount);
+        return new Franc(amount, "CHF");
     }
     
     String currency() {
