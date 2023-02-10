581f8e79c33c7212130006c910535e4ffdc28970
Deory <deory.lee@gmail.com>
2023-02-11 00:15:00 +0900

    5. 중복을 제거하기 위해 리팩토링을 한다. 아무것도 하지 않는 클래스를 없앨까..? 우선 팩토리 메서드를 수정한다.

--- src/main/java/com/deory/tdd_byexample/currency/Money.java
+++ src/main/java/com/deory/tdd_byexample/currency/Money.java
@@ -26,12 +26,12 @@ public class Money {
         return amount + " " + currency;
     }
     
-    static Dollar dollar(int amount) {
-        return new Dollar(amount, "USD");
+    static Money dollar(int amount) {
+        return new Money(amount, "USD");
     }
     
-    static Franc franc(int amount) {
-        return new Franc(amount, "CHF");
+    static Money franc(int amount) {
+        return new Money(amount, "CHF");
     }
     
     String currency() {
