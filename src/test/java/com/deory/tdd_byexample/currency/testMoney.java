ed8439a22fa66ea6eea623237bf1948b24ba940f
Deory <deory.lee@gmail.com>
2023-02-12 21:56:42 +0900

    Bank.reduce 의 가짜 구현이 실패하도록 새로운 테스트를 추가했다. 이제 reduce 는 Sum 을 인자로 받아 축약한 결과로 Money 를 반환해야 한다. 테스트를 추가하고 (testReduceSum) 테스트가 실패하는 것을 확인하고 조금 수정하고 (Bank.reduce) 테스트가 성공하는 것을 확인했다.

--- src/test/java/com/deory/tdd_byexample/currency/testMoney.java
+++ src/test/java/com/deory/tdd_byexample/currency/testMoney.java
@@ -58,4 +58,12 @@ public class testMoney {
         assertEquals(five, sum.augend);
         assertEquals(five, sum.addend);
     }
+    
+    @Test
+    public void testReduceSum() {
+        Expression sum = new Sum(Money.dollar(3), Money.dollar(4));
+        Bank bank = new Bank();
+        Money reduced = bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(7), reduced);
+    }
 }
--- src/test/java/com/deory/tdd_byexample/currency/testMoney.md
+++ src/test/java/com/deory/tdd_byexample/currency/testMoney.md
@@ -3,3 +3,4 @@ Money Test
 1. 5 USD + 10 CHF = 10 USD (2:1 일 경우)
 2. __5 USD + 5 USD = 10 USD__
 3. 5 USD + 5 USD 에서 Money 반환하기
+4. Bank.reduce(Money)
