6fcc703ec7fb647576c1ffc44199d108e8e4f28c
Deory <deory.lee@gmail.com>
2023-02-13 14:42:09 +0900

    Sum.plus() 를 완성하기 위해 테스트를 작성하고 컴파일되게 하고 테스트가 실패하면 조금 수정하고 테스트가 성공했다.

--- src/test/java/com/deory/tdd_byexample/currency/testMoney.java
+++ src/test/java/com/deory/tdd_byexample/currency/testMoney.java
@@ -101,4 +101,15 @@ public class testMoney {
         );
         assertEquals(Money.dollar(10), result);
     }
+    
+    @Test
+    public void testSumPlusMoney() {
+        Expression fiveBucks = Money.dollar(5);
+        Expression tenFrancs = Money.franc(10);
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Expression sum = new Sum(fiveBucks, tenFrancs).plus(fiveBucks);
+        Money result = sum.reduce(bank, "USD");
+        assertEquals(Money.dollar(15), result);
+    }
 }
